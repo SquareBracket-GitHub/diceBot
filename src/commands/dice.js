b0f0aabe0b46624abeb0edfb2dd37df678f231a7
pixel-punk-github <eomtaegyun08@gmail.com>
2024-06-08 02:26:38 +0900

    1.2.0

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,74 +1,102 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const crypto = require("crypto");
+const moment = require("moment");
 
 module.exports = {
     /* Create a new SlashCommandBuilder object. */
     data: new SlashCommandBuilder()
         .setName('주사위')
         .setDescription('주사위를 굴립니다.')
-        .addSubcommand(subCommand => subCommand
-            .setName('d2')
-            .setDescription('d2 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
+        .addStringOption(option => option
+            .setName('type')
+            .setDescription('주사위의 종류를 선택합니다.')
+            .addChoices([
+                {name: 'd2', value: 'd2'},
+                {name: 'd4', value: 'd4'},
+                {name: 'd6', value: 'd6'},
+                {name: 'd10', value: 'd10'},
+                {name: 'd20', value: 'd20'},
+                {name: 'dp', value: 'dp'},
+                {name: 'd120', value: 'd120'}
+            ])
+            .setRequired(true)
         )
-        .addSubcommand(subCommand => subCommand
-            .setName('d4')
-            .setDescription('d4 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
+        .addNumberOption(option => option
+            .setName('times')
+            .setDescription('주사위를 굴릴 횟수를 정합니다.')
+            .setMinValue(1)
+            .setMaxValue(10)
         )
-        .addSubcommand(subCommand => subCommand
-            .setName('d6')
-            .setDescription('d6 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
+        .addBooleanOption(option => option
+            .setName('ephemeral')
+            .setDescription('True로 설정할 경우 주사위 결과가 자신에게만 보여집니다.')
         )
-        .addSubcommand(subCommand => subCommand
-            .setName('d10')
-            .setDescription('d10 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
-        )
-        .addSubcommand(subCommand => subCommand
-            .setName('d20')
-            .setDescription('d20 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
-        )
-        .addSubcommand(subCommand => subCommand
-            .setName('dp')
-            .setDescription('dp(d%) 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
-        )
-        .addSubcommand(subCommand => subCommand
-            .setName('d120')
-            .setDescription('d120 주사위를 굴립니다.')
-            .addNumberOption(option => option
-                .setName('횟수')
-                .setDescription('주사위를 굴릴 횟수를 정합니다.')
-                .setMinValue(1).setMaxValue(10)
-            )
-        ),
+        // .setName('주사위')
+        // .setDescription('주사위를 굴립니다.')
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('d2')
+        //     .setDescription('d2 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('d4')
+        //     .setDescription('d4 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('d6')
+        //     .setDescription('d6 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('d10')
+        //     .setDescription('d10 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('d20')
+        //     .setDescription('d20 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('dp')
+        //     .setDescription('dp(d%) 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        // .addSubcommand(subCommand => subCommand
+        //     .setName('d120')
+        //     .setDescription('d120 주사위를 굴립니다.')
+        //     .addNumberOption(option => option
+        //         .setName('횟수')
+        //         .setDescription('주사위를 굴릴 횟수를 정합니다.')
+        //         .setMinValue(1).setMaxValue(10)
+        //     )
+        // )
+        ,
      /* Execute the slash command when it is called by a user. */
     async execute(interaction) {
         function rollsNumber(num) {
@@ -85,24 +113,33 @@ module.exports = {
             dp: 100,
             d120: 120
         }
-        const diceType = interaction.options._subcommand;          //돌릴 주사위 종류 정하기
-        let rolls; //돌릴 횟수
-        if (!interaction.options._hoistedOptions[0]) rolls = 1;    //몇 번 돌릴지에 대한 값이 없으면 rolls=1
-        else rolls = interaction.options._hoistedOptions[0].value;
+
+        const options = [
+            {name: 'type', value: null}, //돌릴 주사위
+            {name: 'times', value: 1}, //돌릴 횟수
+            {name: 'ephemeral', value: false}
+        ];
+
+        const interactionOptions = interaction.options._hoistedOptions;
+
+        for (i=0; i<=options.length-1; i++) {
+            interactionOptions.forEach(async option => {
+                if (option.name == options[i].name) options[i].value = option.value;
+            });
+        };
         
         let rolledNumber = 0;                                      //돌린 주사위 값들의 총합 변수
         let rolledArr = [];
 
-        for (i=0; i<rolls; i++) {                                  //rolls 값만큼 반복해서 난수 생성
+        for (i=0; i<options[1].value; i++) {                                  //rolls 값만큼 반복해서 난수 생성
             const randomBytes = crypto.randomBytes(1);
-            const random = (randomBytes[0] % dices[diceType]) + 1; //crypto로 생성한 난수
-            // const random = crypto.randomInt(1, dices[diceType] + 1);
+            const random = (randomBytes[0] % dices[options[0].value]) + 1; //crypto로 생성한 난수
             rolledArr.push(random);                                //생성된 난수 rolledArr에 저장
             rolledNumber += random;                           
         }
 
         var embedDescription;                                      //임배드 Description
-        if (rolls > 1) {
+        if (options[1].value > 1) {
             var string = '';
             rolledArr.forEach((num, i) => {                        //여러번 돌렸을 시의 임배드 Description
                 string += '`' + num + '`';
@@ -113,12 +150,12 @@ module.exports = {
         } else embedDescription = `>>> **${rolledNumber}**`;       //한 번 돌렸을 시의 임배드 Description
 
         const embed = new EmbedBuilder()
-            .setAuthor({ name: interaction.user.username + '님의 ' + rollsNumber(rolls) + diceType, iconURL: interaction.user.avatarURL()})
+            .setAuthor({ name: interaction.user.username + '님의 ' + rollsNumber(options[1].value) + options[0].value, iconURL: interaction.user.avatarURL()})
             .setDescription(embedDescription)
             .setColor('#F8F1C8');
         
-        await interaction.reply({ content: '타라락.', embeds: [embed] });
-        console.log(`${interaction.user.username} rolled the ${rollsNumber(rolls)}${diceType} dice, and the number that came up was ${rolledNumber}.`);
+        await interaction.reply({ content: '타라락.', embeds: [embed], ephemeral: options[2].value });
+        console.log(`[${moment(new Date()).format('YY/MM/DD hh:mm')}] ${interaction.user.username} | ${rollsNumber(options[1].value)}${options[0].value} | ${rolledNumber} | ${options[2].value}`);
         // ex) Steve rolled the d20 dice, and the number that came up was 12.
     }
 }
